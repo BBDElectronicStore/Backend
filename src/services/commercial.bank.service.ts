23747b4d0ea67617f5d86ab35690e57ce54ed292
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-06-30 10:10:43 +0200

    Updated bank logic

--- src/services/commercial.bank.service.ts
+++ src/services/commercial.bank.service.ts
@@ -2,7 +2,36 @@ export class CommercialBankService {
     url = 'https://api.commercialbank.projects.bbdgrad.com';
 
     async getCurrentBalance(): Promise<number> {
-        return 5000;
+        try {
+            const response = await fetch(`${this.url}/account/balance`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            return response.json();
+        }
+        catch (e) {
+            console.log(e);
+            return -1;
+        }
     }
+    async getListOfTransactions(): Promise<any> {
+        try {
+            const response = await fetch(`${this.url}/transactions`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            
+            return response.json();
+        }
+        catch (e) {
+            console.log(e);
+            return [];
+        }
+    }
+
 
 }
