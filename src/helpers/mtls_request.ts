695125c86290b11beb35a226bb4ba25c610a82a5
Luke-Bradford-BBD <luke.bradford@projectolympus.co.za>
2024-07-02 18:05:52 +0200

    Remove commented out code

--- src/helpers/mtls_request.ts
+++ src/helpers/mtls_request.ts
@@ -1,19 +1,14 @@
 import https from 'https';
-import fs from 'fs';
 import axios, { Axios, AxiosResponse } from "axios";
 import { S3Helper } from './secret_helper';
 
 export async function makeSecureRequest(data: any, path: string, method: string, hostname: string) {
     try {
-        // const secretHelper = new SecretHelper();
-        // const { certificate, key } = await secretHelper.getMTLSCredentials();
 
         const s3Helper = new S3Helper();
         const certificate = await s3Helper.getObject(`electronics_retailer.crt`);
         const key = await s3Helper.getObject(`electronics_retailer.key`);
         const httpsAgent = new https.Agent({
-            // cert: fs.readFileSync('./.creds/electronics_retailer.crt', 'utf-8'),
-            // key: fs.readFileSync('./.creds/electronics_retailer.key', 'utf-8'),
             cert: certificate,
             key: key,
         });
