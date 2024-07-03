dace33d0bf560e612eb009d19d78216975f99864
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-07-03 12:28:29 +0200

    fix

--- src/handlers/update_price_lambda.ts
+++ src/handlers/update_price_lambda.ts
@@ -1,31 +1,34 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import {UpdatePriceCommand} from "../commands/updatePrice.command";
 import {ProductRepository} from "../repositories/product.repository";
+import {ZeusService} from "../services/zeus.service";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const command = new UpdatePriceCommand(new ProductRepository());
-    if(!event.body)
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Bad Request' }),
-        };
-    const data = JSON.parse(event.body);
-    if(!data.newPrice || !data.newVAT)
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Bad Request' }),
-        };
-    const result = await command.execute(data.newPrice);
-    if(result === null)
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Bad Request' }),
-        };
-    if(result === -1)
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Internal Server Error' }),
-        };
+    // const command = new UpdatePriceCommand(new ProductRepository());
+    // if(!event.body)
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify({ message: 'Bad Request' }),
+    //     };
+    // const data = JSON.parse(event.body);
+    // if(!data.newPrice || !data.newVAT)
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify({ message: 'Bad Request' }),
+    //     };
+    // const result = await command.execute(data.newPrice);
+    // if(result === null)
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify({ message: 'Bad Request' }),
+    //     };
+    // if(result === -1)
+    //     return {
+    //         statusCode: 500,
+    //         body: JSON.stringify({ message: 'Internal Server Error' }),
+    //     };
+    // const service = new ZeusService();
+    // const price = await service.getPrice();
     return {
         statusCode: 200,
         body: JSON.stringify({ message: 'OK' }),
