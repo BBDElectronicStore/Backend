abf9166331d02cba0bdd6a57edc269e2e8e54c0a
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-07-03 14:58:35 +0200

    place order logic

--- src/commands/placeOrder.command.ts
+++ src/commands/placeOrder.command.ts
@@ -7,16 +7,16 @@ export class PlaceOrderCommand implements ICommand<Promise<{ order_id: number, t
     constructor(private readonly repository: OrderRepository) {}
 
     async execute(customerId: number, quantity: number, time: string) {
-        console.log(customerId, quantity);
-        if(this.validate(customerId, quantity, time))
-            return await this.repository.placeOrderAndGetTotalCost(quantity, customerId, time);
+        const q = quantity || 1;
+        console.log(customerId, q);
+        if(this.validate(customerId, q, time))
+            return await this.repository.placeOrderAndGetTotalCost(q, customerId, time);
         return null;
     }
 
     validate(customerId: number, quantity: number, time: string): boolean {
-        // if(!customerId || !quantity)
-        //     return false;
-        // return !(customerId < 0 || quantity < 0);
-        return true;
+        if(!customerId || !quantity)
+            return false;
+        return !(customerId < 0 || quantity < 0);
     }
 }
