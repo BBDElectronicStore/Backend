dc70eb1028305531b660ccae723f7e7ca9e0ceeb
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-07-03 12:57:46 +0200

    change db setup

--- src/database/database.pool.ts
+++ src/database/database.pool.ts
@@ -1,8 +1,12 @@
 import { Pool } from 'pg';
 
 export const DBPool = new Pool({
-    connectionString: process.env.DB_CONNECTION_STRING,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    host: process.env.DBHOST,
+    port: Number(process.env.DBPORT) || 5432,
+    database: process.env.DBNAME,
     ssl: {
         rejectUnauthorized: false
     }
-});
+})
